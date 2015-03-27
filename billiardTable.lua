b31b8345b9db6427debe956fb960a2d6d6378ee7
Dipti Gandhi <gandhidipti.s@gmail.com>
2015-03-27 13:45:04 +0530

    Mostly the changes are made to organize the code

--- billiardTable.lua
+++ billiardTable.lua
@@ -1,3 +1,5 @@
+
+
 require "box2d"
 billiardTable = Core.class(Sprite)
 
@@ -5,29 +7,33 @@ billiardTable = Core.class(Sprite)
 function billiardTable:init()
 	self.table = Bitmap.new(Texture.new("img/billiard_table.png"))
 	self:addChild(self.table)            -- adds the table object to instance of billiardTable 
-	
+	--[[Lets create the physics properties of table like cusion for rebounding and pockets to hold the ball ]]
 	--lets create a physics world of billiard game
 	self.physicsWorldOfbilliard = b2.World.new(0, 0, true)
 	
-	local tableWidth,tableHeight,railWidth,railHeight
+	local tableWidth,tableHeight,railWidth,railHeight 
 	tableWidth =self.table:getWidth()
 	tableHeight = self.table:getHeight()
-	railHeight  = 50   -- minimum height and width of the rail
-	railWidth  = 50
+	railHeight  = 63   -- minimum height and width of the rail
+	railWidth  = 63
+	print("Width and height of the table" , tableWidth , tableHeight)
+	--lets create table rails or cusion to stop the ball going off the table and for rebounding
+	local toprail = self:interactiveRails(100,63,0 , 458)
+	local toprailR = self:interactiveRails(638,63,0,470)
+	local bottomrail = self:interactiveRails(542,621,3.1415 ,455)
+	local bottomrail = self:interactiveRails(1094 , 621 , 3.1415 , 470 )
+	local footrail = self:interactiveRails(71,581,-1.5707,488)
+	local headrail = self:interactiveRails(1123 , 104 , 1.5707 ,486 )
 	
-	--lets create table rails to stop the ball going off the table.
-	--rail(x,y,width,height) for reference
-	local footrail = self:rail(50,tableHeight/2,railWidth,686)
-	local toprail = self:rail(tableWidth/2 , 50 , 1200 , railHeight)
-	local bottomrail = self:rail(tableWidth/2, 636 , 1200, railHeight )
-	local headrail  = self:rail(1150, tableHeight/2 , railWidth,686)  
 	
-	--To give the physics property to rails such as collision detection we need to 
-	--create box2d identity of the above created rails and stage it 
-	self:addChild(self:interactiveRails(footrail))
-	self:addChild(self:interactiveRails(toprail))
-	self:addChild(self:interactiveRails(bottomrail))
-	self:addChild(self:interactiveRails(headrail))
+	-- also make the pocket of the table active in physics world
+	self:createpockets (55,55)
+	self:createpockets(592 , 55)
+	self:createpockets(1143 , 55)
+	self:createpockets(55,628)
+	self:createpockets(590,628)
+	self:createpockets(1143,631)
+	print("Printing Corner Pockets valuse " , cornerPockets)
 	
 	--just for reference, debug draw is very useful, to see the things happening in physics world
 	local debugDraw = b2.DebugDraw.new()
@@ -37,47 +43,31 @@ function billiardTable:init()
 
 end
 
-function billiardTable:rail(x,y,width, height )
-	print("widht : ",width ," height : ",height , " x : " , x , "y : " , y)
-	--shape of the rail is rectangle
-	local railshape = Shape.new()
-	--print("shape AnchorPoint : " , shape:getAnchorPoint())
-	
-	railshape:setLineStyle(5, 0xff0000, 1)
-	railshape:beginPath()
-	railshape:moveTo(-width/2, -height/2)   --shape : moveTo()
-	railshape:lineTo(width/2,-height/2)
-	railshape:lineTo(width/2, height/2)
-	railshape:lineTo(-width/2,height/2)
-	railshape:closePath()
-	railshape:endPath()
-	railshape:setPosition(x,y)
-	print("X and Y position of railshape " , railshape:getPosition())
-	
-	return railshape
-end
 
-function billiardTable : interactiveRails(rail)
-	
-	local railProperty = self.physicsWorldOfbilliard:createBody{type = b2.STATIC_BODY}
-	railProperty:setPosition(rail:getX(),rail:getY())
+
+function billiardTable : interactiveRails(x,y,angle,maxi)
+	local minimum = maxi-15
+	local emptyRailbody = self.physicsWorldOfbilliard :createBody{type = b2.STATIC_BODY}
+	emptyRailbody:setPosition(x,y)
+	emptyRailbody:setAngle(angle)
 	
 	local railShape = b2.PolygonShape.new()
-	railShape:setAsBox(rail:getWidth()/2 , rail :getHeight()/2)
+	railShape:set(-15,-28, maxi,-28,minimum,0, 0,0)       --- -10,-20, 465,-20,455,0, 0,0
 
-	local attachRailshapetobody = railProperty:createFixture{shape = railShape ,density = 1, friction = 1, restitution = 0.1 }
-	
-	rail.body = body
-	
-	return rail
-
+	-- let give shape to the rail body and define other properties of the rail
+	local railFixture = emptyRailbody:createFixture{shape = railShape ,density = 1, friction = 1, restitution = 0.1 }
+	--railFixture:setFilterData({categoryBits = 1 , maskbits = 3, groupIndex = -1})
+	--rail.body = body
+	local reactiveRail = emptyRailbody
+	return reactiveRail
 end
 
 
-function billiardTable:loophole(x,y)
-	local body = self.physicsWorldOfbilliard:createBody{type = b2.STATIC_BODY }
+function billiardTable:createpockets(x,y)
+	self.body = self.physicsWorldOfbilliard:createBody{type = b2.STATIC_BODY }
 	local circle = b2.CircleShape.new(x, y, 38)
-	local fixture = body:createFixture{shape = circle, density = 500.0, 
+	local fixture = self.body:createFixture{shape = circle, density = 500.0, 
 	friction = 1, restitution = 0.8}
-
+	fixture:setFilterData({categoryBits = 1 , maskbits = 1, groupIndex = -1})
+	return body
 end
