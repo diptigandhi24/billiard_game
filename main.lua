917e5311b8ad4b214917f656f38fb2192d53836d
Dipti Gandhi <gandhidipti.s@gmail.com>
2015-04-05 20:32:16 +0530

    not permannent change, just trial and error

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-
+--not going to add  billiardstick
 local billiardobj = billiardTable.new()
 print("table" , billiardobj)
 print ("table.world " ,billiardobj.physicsWorldOfbilliard)
@@ -7,3 +7,19 @@ stage:addChild(billiardobj)
 local ball = cueBallProjection.new(billiardobj.physicsWorldOfbilliard)
 stage:addChild(ball)
 
+local x = 1220  y = 100
+for i=1 ,15 do
+
+	local forcestrip = Shape.new()
+	forcestrip:setLineStyle(2 ,0xff0000 )
+	forcestrip:beginPath()
+	forcestrip:moveTo(x , y)
+	forcestrip:lineTo(x+50 ,y)
+	forcestrip:lineTo(x+50 ,y+10)
+	forcestrip:lineTo(x ,y+10)
+	forcestrip:closePath()
+	forcestrip:endPath()
+
+	y = y+15
+	stage:addChild(forcestrip)
+end
