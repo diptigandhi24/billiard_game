bd0b7c5e00126a8e9fbb3e23813e01a530038590
Dipti Gandhi <gandhidipti.s@gmail.com>
2015-04-08 16:50:35 +0530

    Organization of main
    Main is organised so that it gives the idea of the flow of the entire
    programm.

--- main.lua
+++ main.lua
@@ -1,14 +1,24 @@
 --not going to add  billiardstick
-local billiardobj = billiardTable.new()
-print("table" , billiardobj)
-print ("table.world " ,billiardobj.physicsWorldOfbilliard)
-stage:addChild(billiardobj)
+require "box2d"
+--lets create an interactive world , where all the objects/actors of the game will be added
+local physicsWorldOfbilliard = b2.World.new(0, 0, true)
 
-local ball = cueBallProjection.new(billiardobj.physicsWorldOfbilliard)
-stage:addChild(ball)
+local createTable = billiardTable.new(physicsWorldOfbilliard)
+stage:addChild(createTable)
 
-local x = 1220  y = 100
-for i=1 ,15 do
+local createball = billiardBalls.new(physicsWorldOfbilliard)
+stage:addChild(createball)
+--cueball projection will be created in createball class
+--onEnterFrame will be created in billiardBalls class
+--cueballprojection function will be added in createBalls class
+
+
+
+
+
+
+
+--[[for i=1 ,15 do
 
 	local forcestrip = Shape.new()
 	forcestrip:setLineStyle(2 ,0xff0000 )
@@ -23,3 +33,4 @@ for i=1 ,15 do
 	y = y+15
 	stage:addChild(forcestrip)
 end
+]]--
