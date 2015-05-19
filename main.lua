10a56007eef01c1ec4c031591dc068284ec07356
Dipti Gandhi <gandhidipti.s@gmail.com>
2015-05-19 12:44:12 +0530

    Trail and Error , nothing important

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 --not going to add  billiardstick
+
 require "box2d"
 --lets create an interactive world , where all the objects/actors of the game will be added
 
@@ -9,10 +10,6 @@ stage:addChild(createTable)
 
 local createball = billiardBalls.new(createTable.table)
 stage:addChild(createball)
---print("Child of Billiard balls " , parentSprite:getNumChildren())
---cueball projection will be created in createball class
---onEnterFrame will be created in billiardBalls class
---cueballprojection function will be added in createBalls class
 
 
 
@@ -20,19 +17,4 @@ stage:addChild(createball)
 
 
 
---[[for i=1 ,15 do
 
-	local forcestrip = Shape.new()
-	forcestrip:setLineStyle(2 ,0xff0000 )
-	forcestrip:beginPath()
-	forcestrip:moveTo(x , y)
-	forcestrip:lineTo(x+50 ,y)
-	forcestrip:lineTo(x+50 ,y+10)
-	forcestrip:lineTo(x ,y+10)
-	forcestrip:closePath()
-	forcestrip:endPath()
-
-	y = y+15
-	stage:addChild(forcestrip)
-end
-]]--
