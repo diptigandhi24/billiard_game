b6ed2cad4a2cc3cba96ca590abc5e8c82ff4acbb
Dipti Gandhi <gandhidipti.s@gmail.com>
2015-05-19 12:42:37 +0530

    Reduce the burden of maintaining the objects in this class
    We are not creating the  cueballPositionPreview and
    cueballNotPlaceableIndicator in this class anymore. So we can say that
    this class  keeps the track of only adding and removing billiard balls .
    And all the activities related to the billiard balls.
    
    Print  statements are remove .
    More commentsare added to makethe class unstandable .

--- billiardBallls.lua
+++ billiardBallls.lua
@@ -9,7 +9,7 @@ REFERENCE_WALLSMASK = 8
 PreView_Mask = 16
 
 function billiardBalls : init(BitmapOfTable)
-
+	
 	local X_INTIAL_CUEBALLPOSITION = 400
 	local Y_INTIAL_CUEBALLPOSITION = 400
 	
@@ -31,107 +31,101 @@ function billiardBalls : init(BitmapOfTable)
 	self:addChild(self.coloredBalls2)
 	self.coloredBalls2.name = "coloredBalls"
 	
-	--create the projection of cueball and add it to the stage 
+	--creates the projection of cueball and add it to the billiard class.
+	self.billiardTableObj = BitmapOfTable
+	self.projectionObj = cueBallProjection.new( self.cueBall ,self.billiardTableObj )
+	self:addChild( self.projectionObj )
 	
-	self.projectionObj = cueBallProjection.new( world, self.cueBall , BitmapOfTable)
-	self:addChild(self.projectionObj)
+	--when world bodies comes in contact with eachother perform the following events
+	world:addEventListener( Event.BEGIN_CONTACT, self.onContact ,self )
+	world:addEventListener( Event.END_CONTACT , self.endContact ,self)
 	
-
--- update the position of ball every frame
-	--When any ball comes in contact with  pockets  , delete the ball
-	
-	world:addEventListener(Event.BEGIN_CONTACT, self.onContact ,self )
-	world:addEventListener(Event.END_CONTACT , self.endContact ,self)
-	
-	-- update the position  of ball every frame
-	--Also you can add and remove the ball only in onEnterFrame function , as world update itself in it.
-	self:addEventListener(Event.ENTER_FRAME, self.onEnterFrame , self)
-	
-	-- The two images added below are invisble when the screen is loaded .They are made visible and are used only when the cueball falls in the pocket. 
-	--------- If the cueball ball is deleted reload the ball to user selected place------------
-	-- show the preview of cueball position 
-	self.cueballPositionPreview = Bitmap.new(Texture.new("img/reloadCueball.png"))
-	self.cueballPositionPreview:setPosition(X_INTIAL_CUEBALLPOSITION ,Y_INTIAL_CUEBALLPOSITION)
-	self.cueballPositionPreview:setAnchorPoint(0.5 , 0.5)
-	self:addChild(self.cueballPositionPreview)
-	self.cueballPositionPreview:setVisible(false)
-	
-	--if the user selected place is not correct then show Noentry image
-	
-	self.cueballNotPlaceableIndicator = Bitmap.new(Texture.new("img/noentry.png"))
-	self.cueballNotPlaceableIndicator:setAnchorPoint(0.5,0.5)
-	self.cueballNotPlaceableIndicator:setPosition(X_INTIAL_CUEBALLPOSITION ,Y_INTIAL_CUEBALLPOSITION)
-	self:addChild(self.cueballNotPlaceableIndicator)
-	self.cueballNotPlaceableIndicator:setVisible(false)
-	
+	--update the position  of balls (i.e dynamic bodies)  every frame
+	--Also you can add and remove the ball and its body only  onEnterFrame function , as world update itself in it.
+	self:addEventListener( Event.ENTER_FRAME, self.onEnterFrame , self )
 	
 	print("The number of child billiard balls has in the beginning " , self:getNumChildren())
-	
+		
 end
 
-function billiardBalls:physicsPropertyOfBall(ball)
- 
-	local body = world:createBody{type = b2.DYNAMIC_BODY}
+function billiardBalls : physicsPropertyOfBall( ball )
+	
+	local body = world:createBody{ type = b2.DYNAMIC_BODY }
 	body.name = "ball"
-	body:setPosition(ball:getX(),ball:getY())
+	body:setPosition( ball:getX(),ball:getY())
 	body:setLinearDamping(0.5)
 	body:setAngularDamping(1)
 	
+	--Inner Fixture are the actual shapes of the images in the game and detect and react to collision
 	local innerCircle = b2.CircleShape.new(0,0,20)
-	local innerFixture = body:createFixture{shape = innerCircle, density = 1, friction = 1, restitution = 0.8  }
+	local innerFixture = body:createFixture{ shape = innerCircle, density = 1, friction = 1, restitution = 0.8 }
 	--All the balls will collide with other balls and walls
-	innerFixture:setFilterData{categoryBits = BALLS_MASK, maskBits = BALLS_MASK+WALLS_MASK + PreView_Mask    }
+	innerFixture:setFilterData{ categoryBits = BALLS_MASK, maskBits = BALLS_MASK+WALLS_MASK + PreView_Mask }
 	
-	--outerFixture doesnt collide with any fixture
+	--outerFixture are bigger by the radius of the ball, so that the inner fixtures of objects will just touch each other 
+	--instead of overlapping . And the outer Fixtures  of objects will overlap 
 	local outerCircle = b2.CircleShape.new(0,0,40)
 	local outerfixture = body:createFixture{shape = outerCircle, density = 1, friction = 1, restitution = 0.8 }
 	outerfixture:setFilterData{ categoryBits = REFERENCE_BALLMASK , maskBits = 1 , groupIndex = -3 }
 	
 	ball.body = body
-	ball.fixture = innerFixture
+	ball.fixture = innerFixture  -- for reference
 	
-
 end
 
 function billiardBalls:onContact(e)
+		
 	local fixtureA = e.fixtureA
-		
-		local fixtureB = e.fixtureB
-		
-		local bodyA = fixtureA:getBody()
-		local bodyB = fixtureB :getBody()
-		
-		print("begin contact: "..bodyA.name.."<->"..bodyB.name)
-		if(bodyA.name =="Pockets" and bodyB.name == "ball")then
-			--createball.deleteBody = true
-			--bodyB:setRotation(2.12)
-			bodyB.delete = true
+	local fixtureB = e.fixtureB
+	local bodyA = fixtureA:getBody()
+	local bodyB = fixtureB :getBody()
+	print("begin contact: "..bodyA.name.."<->"..bodyB.name)
+	
+	if( bodyA.name =="Pockets" and bodyB.name == "ball" )then
+	
+		bodyB.delete = true
 
-		end
-		if(bodyA.name == "reload" and bodyB.name =="ball")then
-			self.cueballNotPlaceableIndicator:setVisible(true)
-		end
-
+	end
+	
+	if( bodyA.name == "reload" and bodyB.name =="ball" )then
+	
+		self.reloadobj.cueballNotPlaceableIndicator:setVisible(true)
+		
+	end
+	
+	if( bodyA.name =="Pockets" and bodyB.name == "reload" )then
+	
+		self.reloadobj.cueballNotPlaceableIndicator:setVisible(true)
+		
+	end
+		
 end
 function billiardBalls:endContact(e)
-		print("Contact End ")
-		local fixtureA = e.fixtureA
+
+	local fixtureA = e.fixtureA
+	local fixtureB = e.fixtureB
+	local bodyA = fixtureA:getBody()
+	local bodyB = fixtureB :getBody()
+	
+	if( bodyA.name == "reload" and bodyB.name =="ball" )then
+	
+		self.reloadobj.cueballNotPlaceableIndicator:setVisible(false)
 		
-		local fixtureB = e.fixtureB
+	end
+	
+	if( bodyA.name =="Pockets" and bodyB.name == "reload" )then
+	
+		self.reloadobj.cueballNotPlaceableIndicator:setVisible(false)
 		
-		local bodyA = fixtureA:getBody()
-		local bodyB = fixtureB :getBody()
-		
-		if(bodyA.name == "reload" and bodyB.name =="ball")then
-			self.cueballNotPlaceableIndicator:setVisible(false)
-		end
+	end
 end
 
 function billiardBalls:addCueball(x,y)
+
 	self.cueBall = Bitmap.new(Texture.new("img/cueball.png"))
 	self.cueBall:setAnchorPoint(0.5,0.5)
 	self.cueBall:setPosition( x ,y)
-	self:physicsPropertyOfBall(self.cueBall,world)
+	self:physicsPropertyOfBall(self.cueBall)
 	self:addChild(self.cueBall)
 	self.cueBall.name = "cueBall"
 
@@ -140,63 +134,72 @@ end
 function billiardBalls:onEnterFrame()
 	
 	world:step(1/60, 8,3)
-	local zeroVelocityObjects =1 -- because in lua counting of elements/indexes starts from 1 and not from zero. 
+	local zeroVelocityObjects =0 
 	
---  Reverse for loop in which last in first out (lifo) is applied 
---  for better explaination refer http://howto.oz-apps.com/2011/09/tower-of-babel-no-honoi-maybe.html
+	--Reverse for loop in which last in first out (lifo) is applied 
+	--for better explaination refer http://howto.oz-apps.com/2011/09/tower-of-babel-no-honoi-maybe.html
+	print("NUmber Of Children Billiard has " ,self:getNumChildren())
 	for i = self:getNumChildren() , 1,-1  do
+	
 		--get specific sprite
 		local sprite = self:getChildAt(i)
 		
-			-- check if sprite HAS a body (ie, physical object reference we added)
+		-- check if sprite HAS a body (ie, physical object reference we added)
 		if sprite.body  then
+		
 			--update position to match box2d world object's position
 			--get physical body reference
 			local body = sprite.body
 			--get body coordinates
 			local bodyX, bodyY = body:getPosition()
-			
-			if sprite.name == cueBall and sprite == nil then
-				self:addCueball(400,400)
-			end
-			--If all the objects are at rest then unable the touch events in the game
-			if body:getLinearVelocity() == 0 and body.name == "ball" then
-				zeroVelocityObjects = zeroVelocityObjects + 1
-				if(zeroVelocityObjects == self:getNumChildren())then
+				-- If all the balls are at rest then let the user play , otherwise wait till the balls comes at rest
+				if body:getLinearVelocity() == 0 and body.name == "ball" then
+				
+					zeroVelocityObjects = zeroVelocityObjects + 1
 					
-					self.projectionObj:addEventListener(Event.MOUSE_DOWN , self.projectionObj.onMouseDown , self.projectionObj)
-					self.projectionObj:addEventListener(Event.MOUSE_MOVE, self.projectionObj.onMouseMove,self.projectionObj)
-					self.projectionObj:addEventListener(Event.MOUSE_UP, self.projectionObj.onMouseUp, self.projectionObj)
-					self.projectionObj.projectBall:setVisible(true)
-					world:rayCast(self.projectionObj.cueBall:getX() ,self.projectionObj.cueBall:getY(),self.projectionObj.projectBall:getX(),self.projectionObj.projectBall:getY(), self.projectionObj.raycastCallback ,self.projectionObj)
+					if(zeroVelocityObjects == self:getNumChildren()-1 and self.cueBall ~= nil)then
+					
+						print("ZeroVelocity objects are " ,zeroVelocityObjects)
+						print("NUmber of object with body " ,self:getNumChildren() )
+						self.projectionObj.projectBall:setVisible(true)
+						self.projectionObj:defaultLengthOfRaycast()
+						self.projectionObj:addEventListener(Event.MOUSE_DOWN , self.projectionObj.onMouseDown , self.projectionObj)
+						self.projectionObj:addEventListener(Event.MOUSE_MOVE, self.projectionObj.onMouseMove,self.projectionObj)
+						self.projectionObj:addEventListener(Event.MOUSE_UP, self.projectionObj.onMouseUp, self.projectionObj) 
+						
+					end
+					
 				end
-			end
+			
 			--apply coordinates to sprite
 			sprite:setPosition(bodyX, bodyY)
-			if(sprite.body.delete)then
+			if( sprite.body.delete )then
 				
 				world:destroyBody(body)
 				sprite.body.delete = false
 				self:removeChild(sprite)
 			
 				
-				if sprite.name == "cueBall"  then	--if the deleted ball is cueball 
-					self.reloadobj = ReloadThecueBall.new(self.cueballPositionPreview ,self.cueballNotPlaceableIndicator )
+				if sprite.name == "cueBall"  then	--if the deleted ball is cueball
+					self.cueBall = nil
+					self.reloadobj = ReloadThecueBall.new(self)
+					print("when reload created Preview of cueball" , self.cueballPositionPreview)
 					self:addChild(self.reloadobj)
 					
 				end
 				
-				if sprite.body.name =="reload" then
+				if sprite.body.name =="reload" then 
+				
 					self:addCueball(self.reloadobj.SetX , self.reloadobj.SetY )
+					self.projectionObj.cueBallObj = nil
+					self.projectionObj.cueBallObj =self.cueBall
 					self:removeChild(self.reloadobj)
+					print("when reload remove Preview of cueball" , self.cueballPositionPreview)
+					self.reloadobj = nil
 					
 				end
 				
 			end
-			--if cueball gets deleted from the game then add it again 
-			
-			
-			
 			
 		end
 	end
